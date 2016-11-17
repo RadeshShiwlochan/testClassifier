07f8b7db064d6611e02579e9164634d188e3e3de
radeshshiwlochan <radesh0430@gmail.com>
2016-11-16 22:31:03 -0500

    testes the Bayes classifer of the module natural

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router  = express.Router();
+var natural = require('natural');
+
 
 router.get('/', (req, res, next) => {
+	var classifier = new natural.BayesClassifier();
+	classifier.addDocument('i am long qqqq', 'buy');
+	classifier.addDocument('buy the q\'s', 'buy');
+	classifier.addDocument('short gold', 'sell');
+	classifier.addDocument('sell gold', 'sell');
+	classifier.train();
+
+	console.log(classifier.classify('i am short silver '));
+	console.log(classifier.classify('i am long copper'));
 	res.render('index');
 });
 
